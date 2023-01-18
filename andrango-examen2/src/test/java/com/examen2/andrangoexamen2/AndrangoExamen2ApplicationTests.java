c4060fe78ac29d84660739f5d91f61ab87299552
Ismael Sebastián Andrango Peralbo <51093008+SebastianAndrango@users.noreply.github.com>
2023-01-18 10:36:45 -0500

    Initial Commit

---
+++ andrango-examen2/src/test/java/com/examen2/andrangoexamen2/AndrangoExamen2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen2.andrangoexamen2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndrangoExamen2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
